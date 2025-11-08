1b01fbbf956c464eefa40328ca4891e59776df8b
Shane Clark <170shane@gmail.com>
2025-11-08 07:29:18 +0000

    Implement CRUD operations for todos: add, update, and delete routes

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -36,10 +36,36 @@ def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     return todo
 
 # Sample route to create a new todo
-# @app.post("/todo", response_model=models.Todos_Response, status_code=status.HTTP_201_CREATED)
-# def create_todo(todo_request: models.Todos_Request, db: db_dependency):
-#     todo_model = models.Todos(**todo_request.model_dump())
-#     db.add(todo_model)
-#     db.commit()    
-#     db.refresh(todo_model)
-#     return todo_model
+@app.post("/todo", response_model=models.TodosRequest, status_code=status.HTTP_201_CREATED)
+def create_todo(todo_request: models.TodosRequest, db: db_dependency):
+    todo_model = models.Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()    
+    db.refresh(todo_model)
+    return todo_model
+
+# Sample route to update a todo
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_todo(todo_request: models.TodosRequest, db: db_dependency, todo_id: int = Path(gt=0)) :
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if not todo_model:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    
+    for key, value in todo_request.model_dump().items():
+        setattr(todo_model, key, value)
+    
+    db.add(todo_model)
+    db.commit()
+    return
+
+
+# Sample route to delete a todo
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    if not todo_model:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
+    
+    db.delete(todo_model)
+    db.commit()
+    return
